dac810f1800f6f16b32524552bfe0c527d6fe2ab
Ali Aljendy <alialjndy2@gmail.com>
2025-04-17 21:22:11 +0300

    Add-feature | create Model , Migration and controller for Parent_Student

---
+++ app/Http/Controllers/ParentStudentController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Parent_Student;
+use Illuminate\Http\Request;
+
+class ParentStudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Parent_Student $parent_Student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Parent_Student $parent_Student)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Parent_Student $parent_Student)
+    {
+        //
+    }
+}
