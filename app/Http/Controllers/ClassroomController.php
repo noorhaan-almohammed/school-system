a6e7bf93e26e9bdd807f9236378e54dadb070b49
Ali Aljendy <alialjndy2@gmail.com>
2025-04-17 21:20:06 +0300

    Add-feature | create Model , Migration and controller for classRoom

---
+++ app/Http/Controllers/ClassroomController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Classroom;
+use Illuminate\Http\Request;
+
+class ClassroomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Classroom $classroom)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Classroom $classroom)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Classroom $classroom)
+    {
+        //
+    }
+}
