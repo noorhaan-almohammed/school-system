00e8f712f2de6265973d8a5c6967d010cd16bba2
Ali Aljendy <alialjndy2@gmail.com>
2025-04-19 02:35:49 +0300

    refactor: renamed models and adjusted Eloquent relationships to match new names

---
+++ app/Http/Controllers/OverallPerformancesController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OverallPerformances;
+use Illuminate\Http\Request;
+
+class OverallPerformancesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OverallPerformances $overallPerformances)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OverallPerformances $overallPerformances)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OverallPerformances $overallPerformances)
+    {
+        //
+    }
+}
