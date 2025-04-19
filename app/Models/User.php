f14e251956f2242cfc7de8ad503be2a655a2995a
Ali Aljendy <alialjndy2@gmail.com>
2025-04-19 21:48:52 +0300

    refactor | rename field in Database

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'full_name',
+        'name',
         'email',
         'password',
         'phone_number'
