fa967c0e68aa9e9579a7148fe14592fb59af28f6
noorhaan-almohammed <gnourhhaan1994@gmail.com>
2025-04-19 18:57:08 +0300

    install laravel ui

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('/admin', function () {
     return view('admin.admin');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
