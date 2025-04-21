d529add4e1f95684cd36faa597262e27b836bd76
Ali Aljendy <alialjndy2@gmail.com>
2025-04-21 03:26:13 +0300

    add-feat | create ClassRoom Seeder and update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PermissionSeeder::class,
             RoleSeeder::class,
+            SubjectSeeder::class,
+            ClassRoomSeeder::class,
         ]);
     }
 }
