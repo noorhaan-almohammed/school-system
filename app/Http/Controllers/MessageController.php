a28591a89ba1db828e6fa514065402b96bb87366
Batool <bbaty.193@gmail.com>
2025-04-23 00:33:11 +0300

    add feat | Message controller,service,request <get new messages>

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\message;
 use Illuminate\Http\Request;
 use App\Services\MessageService;
+use App\Http\Resources\MessageResource;
 
 class MessageController extends Controller
 {
@@ -28,6 +30,11 @@ class MessageController extends Controller
     {
         $Message = $this->MessageService->sendMessage($request->validated());
         return self::success(null, 'Message Sent Successfully',201);
+    }
+    public function getNewMessages(User $reciever)
+    {
+        $message = $this->MessageService->getNewMessages($reciever);
+        return self::success(new MessageResource($message), 'Message Retrieved Successfully',200);
     }
 
     /**
