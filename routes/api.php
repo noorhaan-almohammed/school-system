0b2909f4b15a871adeee024ba3f4dae07d64a77f
noorhaan-almohammed <gnourhhaan1994@gmail.com>
2025-05-13 01:10:19 +0300

    finish

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,14 @@ use App\Http\Controllers\UserController;
 Auth::routes();
 //auth api
 Route::post('/login',[AuthController::class,'login']);
-Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/users/{id}', [UserController::class, 'showUser']);
+    Route::post('/logout',[AuthController::class,'logout']);
+    Route::get('/me', function () {
+        return response()->json([
+            'user' => Auth::user()
+        ]);
+    });
     Route::get('/users', [UserController::class, 'showUserInfoBacedOnRole']);
 });
 
